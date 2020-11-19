fad5860ce3dccfcdda45041f5df7e7abc6a74d55
DESKTOP-4TVOSME\umikainonoka <18141017@edu.cc.saga-u.ac.jp>
2020-11-19 10:53:41 +0900

    紫が作りました

---
+++ app/java.js
@@ -0,0 +1,3 @@
+document.getElementById("Login").addEventListener("click", function () {
+    location.replace("file:///C:/Users/umikainonoka/Videos/Documents/09_A/app/%E4%BF%9D%E8%82%B2%E4%BE%9D%E9%A0%BC%E7%94%BB%E9%9D%A2.html");
+  }, false);
